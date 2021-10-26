ae0b4f271accb44f40e166f854db5d22cf01eb01
xian zhang <imzhangxian@gmail.com>
2021-10-26 16:28:19 +0800

    consider processing time

--- src/waterSupplySim.py
+++ src/waterSupplySim.py
@@ -13,13 +13,18 @@ ELECTRICITY_PRICE = [0.2695, 0.2695, 0.2695, 0.2695, 0.2695, 0.2695, 0.2695, 0.5
         0.8366, 0.8366, 0.8366, 0.8366, 0.8366, 0.5615, 0.5615, 0.5615, 0.5615, 0.5615, 1.0107, 1.0107, 
         0.5615, 0.2695]
 STORAGE_CAPACITY = 2
+INITIAL_STORAGE = 0.8
 STORAGE_ALERT_THRESHOLD = 0.5
+STORAGE_SAFETY_LINE = 1
+
+INTAKE_CEILING = 2
 DTIME = 10 # dt - computational interval
 PREDICTION_LEAD = 60 # prediction lead time (T)
+PROCESSING_TIME = 60
 
 # predicted the water demand of (t, t + lead-time) based on demand curve
-def hourlyPrediction(t):
-    PRED_SLICES = math.floor(PREDICTION_LEAD / DTIME)
+def predictDemand(t, duration = 60):
+    PRED_SLICES = math.floor(duration / DTIME)
     d = 0
     for i in range(PRED_SLICES):
         hr = math.floor(t / 60)
@@ -31,13 +36,17 @@ def hourlyPrediction(t):
 # max speed of next prediction period: strategy for valley price
 def getMaxSpeed(demand, waterstore):
     speed = (STORAGE_CAPACITY - waterstore + demand)
-    return speed if speed <= 2 else 2
+    return speed if speed <= INTAKE_CEILING else INTAKE_CEILING
 
 # min speed to keep the level over alert threshold in next prediction period
 def getMinSpeed(demand, waterstore):
-    deficit = (demand + STORAGE_ALERT_THRESHOLD - waterstore)
+    deficit = (demand + STORAGE_SAFETY_LINE - waterstore)
     speed = deficit
-    return speed if speed > 0 else 0
+    if speed < 0:
+        speed = 0
+    if speed > INTAKE_CEILING:
+        speed = INTAKE_CEILING
+    return speed
 
 # electricity pricing at time t
 def electricityPrice(t):
@@ -48,20 +57,21 @@ def electricityPrice(t):
 # speed adjustment algorithm
 def adjustSpeed(t, demand, storedVol):
     eprice = electricityPrice(t)
-    eprice_delta = electricityPrice(t + 120) - eprice
-    speed = getMaxSpeed(demand, storedVol) if eprice_delta > 0 else getMinSpeed(demand, storedVol)
+    eprice_delta = electricityPrice(t + 60) - eprice
+    demand_after_delay = predictDemand(t + PROCESSING_TIME)
+    speed = getMaxSpeed(demand_after_delay, storedVol) if eprice_delta > 0 else getMinSpeed(demand_after_delay, storedVol)
     return speed
 
 def simulateDemandSupply():
     print('simulate demand supply curve ... ')
-    inventory = 0.5
+    inventory = INITIAL_STORAGE
     intakes = []
     inventories = []
     dtimes = []
     eprices = []
     demands = []
     for t in range(DTIME, 60 * 24, DTIME):
-        demand = hourlyPrediction(t)
+        demand = predictDemand(t)
         intake = adjustSpeed(t, demand, inventory)
         # print('time at: ', math.floor(t/60), ':', t % 60, ', speed: ', 
         #        in_speed, ', volume: ', currentStorageVol)
@@ -70,7 +80,10 @@ def simulateDemandSupply():
         dtimes.append(t/60)
         demands.append(demand)
         eprices.append(electricityPrice(t))
-        inventory = inventory + (intake - demand) * DTIME / 60
+        # supply should be PROCESSING_TIME slower than intake
+        k = math.floor((t - PROCESSING_TIME) / DTIME)
+        supply = intakes[k if k > 0 else 0]
+        inventory = inventory + (supply - demand) * DTIME / 60
 
     plt.plot(dtimes, intakes, label='Pump intake')
     plt.plot(dtimes, inventories, label='Inventory')
